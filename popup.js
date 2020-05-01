6a6ee1597eb1f55060ea375c16b0252eeec4fd54
Yatin Gupta <yatingupta7777@gmail.com>
2020-05-01 16:24:23 +0530

    Created list with delete button

--- popup.js
+++ popup.js
@@ -1,30 +1,64 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  var text_area = document.getElementById('get_url');
+  
+  var addItemButton = document.getElementById('addItemButton');
+  var textItemInput = document.getElementById('textItemInput');
 
-  checkPageButton.addEventListener('click', function() {
+  var textList = document.getElementById('textList');
 
-    chrome.tabs.getSelected(null, function(tab) {
-      //alert(tab.url)
-      chrome.storage.sync.set({'URL': tab.url}, function() {
-          // Notify that we saved.
-         // message('Settings saved');
-          alert('Settings saved');
-          text_area.innerHTML = tab.url;
-          //google tracking here
+  var textsList = new Array();
+
+  chrome.storage.sync.get(['list1'], function (result) {
+
+      if(result.list1 !=undefined){
+            textsList = result.list1;
+            for (let key in textsList)
+            {
+                addItem(textsList[key]);
+            }
+      }
+  })
+
+  addItemButton.addEventListener('click', function() {
+
+    var inputValue = textItemInput.value;
+    if(inputValue == ''){
+      chrome.tabs.getSelected(null, function(tab) {
+        addItem(tab.url);
+        textsList.push(tab.url)
+        chrome.storage.sync.set({'list1': textsList}, function() {
+          });
+      });
+
+    }else{
+        addItem(inputValue);
+        textsList.push(inputValue);
+        chrome.storage.sync.set({'list1': textsList}, function() {
         });
-    });
+    }
+
   }, false);
 
 
+  function addItem(value){
+    var div = document.createElement("DIV");          
+    var para = document.createElement("P");    
+    var deleteButton = document.createElement("BUTTON");                   // Create a <p> node
+    var t = document.createTextNode(value);      // Create a text node
+    
+    para.style.width="50px";
+    para.appendChild(t); 
+    div.appendChild(para);
+    div.appendChild(deleteButton);
+    textList.appendChild(div);
+  }
   //Storage
-  chrome.storage.sync.get("URL", function(result){
-    if(result.URL!=undefined){
-      text_area.innerHTML = result.URL;
-    }else{
-      text_area.innerHTML = "HI";
-    }
-    }); 
+  // chrome.storage.sync.get("list1", function(result){
+  //   if(result.URL!=undefined){
+  //     text_area.innerHTML = result.URL;
+  //   }else{
+  //     text_area.innerHTML = "HI";
+  //   }
+  //   }); 
 
   //Can be used if listener needed
 
