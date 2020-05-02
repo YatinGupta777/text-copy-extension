d090d92f0f7956fcda7c9deda9e15c213678b194
Yatin Gupta <yatingupta7777@gmail.com>
2020-05-02 14:19:56 +0530

    Removed unnecessary code

--- popup.js
+++ popup.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
   var addItemButton = document.getElementById('addItemButton');
   var textItemInput = document.getElementById('textItemInput');
 
-  //var chromeStorage = 'list2';
   var elementsList = document.getElementById('elementsList');
 
   var textsList = new Array();
@@ -42,32 +41,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
   }, false);
 
-
-  // function addItem(value){
-  //   var li = document.createElement("LI");          
-  //   var para = document.createElement("P");    
-  //   var deleteButton = document.createElement("BUTTON");                   // Create a <p> node
-  //   var t = document.createTextNode(value);      // Create a text node
-    
-  //   deleteButton.className = "delete";
-
-  //   para.appendChild(t); 
-  //   li.appendChild(para);
-  //   li.appendChild(deleteButton);
-  //   elementsList.appendChild(li); 
-
-  //   $(deleteButton).click(function () {
-  //               var index = $(this).index(".delete");
-  //               alert(index);
-  //               var li = this.parentElement;
-  //               li.style.display = "none";
-  //               removeItem(index);  //This function just removes it from chrome local storage
-  //               $(".delete").eq(index).remove();
-  //           })
-  //   }
-
 function addItem(value) { 
-  $(elementsList).append(`<li><p>${value}</p><button class="delete"></button></li>`);
+  $(elementsList).append(`<li><p>${value}</p><button class="delete">\u00D7</button><button class="alert">AlertText</button></li>`);
 }
 
 jQuery($ => {
@@ -75,6 +50,12 @@ jQuery($ => {
     let $button = $(this);
     removeItem($button.index('.delete'));
     $button.closest('li').remove();
+  });
+
+  $(elementsList).on('click', '.alert', function() {
+    let $button = $(this);
+    let copyFrom = $button.siblings('p')[0];
+    alert(copyFrom.innerText)
   });
 });
 
@@ -91,25 +72,7 @@ jQuery($ => {
                 })
 
             })
-    }
-  //Storage
-  // chrome.storage.sync.get("list1", function(result){
-  //   if(result.URL!=undefined){
-  //     text_area.innerHTML = result.URL;
-  //   }else{
-  //     text_area.innerHTML = "HI";
-  //   }
-  //   }); 
-
-  //Can be used if listener needed
-
-  // chrome.storage.onChanged.addListener(function(changes, namespace) {
-  //       for (var key in changes) {
-  //         var storageChange = changes[key];
-  //         alert(storageChange.URL);
-  //       }
-  //     });
-
+  }
 
 }, false);
 
